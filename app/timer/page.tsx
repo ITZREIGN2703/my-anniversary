1d993696fe4b44b5558820d0be18ca3152b44d5d
ITZREIGN2703 <sovandam6@gmail.com>
2024-03-20 15:27:11 +0700

    add app directory

---
+++ app/timer/page.tsx
@@ -0,0 +1,19 @@
+export default function AnniversaryText() {
+  return (
+    <div className="wrapper">
+      <div className="font-bold text-4xl m-2">My Feeling For You❤️</div>
+      <div className="font-semibold text-2xl m-2">27-03-2023~27-03-2024</div>
+      <hr className="m-4" />
+      <div className="font-medium m-2">
+        The date <strong>27 March 2023</strong> was the day I confessed my love
+        to you through texts. To start off, I would like to say this again and
+        again until we grow old and leave this Planet, I Love You So So Much and
+        I hope we can be together for many more years to come.💕
+        <br />
+        You have to read a lot today so get ready my love.🤣 It has been a year
+        that we have been together, and I feel really lucky having you by my
+        side.
+      </div>
+    </div>
+  );
+}
---
+++ app/waiting/page.tsx
@@ -0,0 +1,97 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import Link from "next/link";
+
+export default function WaitingScreen() {
+  const [partyTime, setPartyTime] = useState(false);
+  const [days, setDays] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const target = new Date("03/20/2024 20:09:59");
+
+    const interval = setInterval(() => {
+      const now = new Date();
+      const difference = target.getTime() - now.getTime();
+
+      const d = Math.floor(difference / (1000 * 60 * 60 * 24));
+      setDays(d);
+
+      const h = Math.floor(
+        (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      setHours(h);
+
+      const m = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+      setMinutes(m);
+
+      const s = Math.floor((difference % (1000 * 60)) / 1000);
+      setSeconds(s);
+
+      if (d <= 0 && h <= 0 && m <= 0 && s <= 0) {
+        setPartyTime(true);
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div>
+      {partyTime ? (
+        <>
+          <div className="h-screen flex items-center flex-col justify-center">
+            <div className="font-bold text-4xl text-center text-center m-20 text-red-700">
+              <video className="rounded-md" width="400" height="320" controls preload="none">
+                <source src="Vod.mp4" type="video/mp4" />
+              </video>
+            </div>
+            <Link href="/timer">
+              <button
+                className="select-none rounded-lg bg-pink-500 py-3 px-6 text-center align-middle font-sans text-2xs font-bold uppercase text-white shadow-md shadow-red-500/20 transition-all hover:shadow-lg hover:shadow-red-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                type="button"
+              >
+                Click Here To Read
+              </button>
+            </Link>
+          </div>
+        </>
+      ) : (
+        <div className="wrapper">
+          <div className="timer-wrapper">
+            <div className="timer-inner">
+              <div className="timer-segment">
+                <span className="time">{days}</span>
+                <span className="label">Days</span>
+              </div>
+              <span className="divider">:</span>
+              <div className="timer-segment">
+                <span className="time">{hours}</span>
+                <span className="label">Hours</span>
+              </div>
+              <span className="divider">:</span>
+              <div className="timer-segment">
+                <span className="time">{minutes}</span>
+                <span className="label">Minutes</span>
+              </div>
+              <span className="divider">:</span>
+              <div className="timer-segment">
+                <span className="time">{seconds}</span>
+                <span className="label">Seconds</span>
+              </div>
+            </div>
+          </div>
+          <div className="font-bold text-4xl m-4 text-center">
+            Time Until Our Anniversary
+          </div>
+          <div className="font-semibol text-2xl m-4 text-center">
+            Come back Here When it is time
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
