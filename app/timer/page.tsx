4b41f52974bfe160d02579b6e420ee0dd99ad28d
ITZREIGN2703 <sovandam6@gmail.com>
2024-03-23 11:28:04 +0700

    more

--- app/timer/page.tsx
+++ app/timer/page.tsx
@@ -15,15 +15,27 @@ export default function AnniversaryText() {
         side.
         <br />
         Being with you, I think it is really lucky for me. Do you remember when
-        we are not together yet and we stay until 8 o&aposclock everyday to just
-        chat with each other? You are my lucky charm because being of that mum
-        thought that I stayed at school to study because I don&apost have laptop
-        and she bought it for me.😂It was really funny how you brought that luck
-        to me sweetheart. After that, we would also continue to chats. I
-        don&apost really know when I fell in love with you but I know for you
-        that right now you fill my heart. We made a lot of memories such as
-        drinking beer together, do you remember that? We also went to the gym
-        together, that was fun.
+        we are not together yet and we stay until 8 o&apos;clock everyday to
+        just chat with each other? You are my lucky charm because being of that
+        mum thought that I stayed at school to study because I don&apos;t have
+        laptop and she bought it for me.😂It was really funny how you brought
+        that luck to me sweetheart. After that, we would also continue to chats.
+        I don;t really know when I fell in love with you but I know for you that
+        right now you fill my heart. We made a lot of memories such as drinking
+        beer together, do you remember that? We also went to the gym together,
+        that was fun. We ate too many thing and I ate too much that&apos;s why I
+        am this fat🤣but it is okay, those time with you were majestic and I
+        really glad I got to spend them with you. We cannot just eat a lot
+        without a care again since I want to lose weight but we will do other
+        things together. Maybe next year or a few more years, we can go far from
+        the city and go to the sea, forest, mountain, etc. You are my
+        heart&apos;s desire😘 so we will spend those time together too and I
+        know it will be really fun. After I confessed to you, I really want to
+        be always with you and it felt like an eternity when I am not with you.
+        I still feel the same now but we are adult now we have responsibilities,
+        we cannot act the same like we used to before. I believe that even
+        though right now we do our own things, we will meet up once again and
+        have fun together. We just have to bear with it until then.
       </div>
     </div>
   );
