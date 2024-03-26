606242905ef3c4a627036f229d62d8a9b7cfb363
ITZREIGN2703 <sovandam6@gmail.com>
2024-03-26 22:27:08 +0700

    more

--- app/timer/page.tsx
+++ app/timer/page.tsx
@@ -20,22 +20,34 @@ export default function AnniversaryText() {
         mum thought that I stayed at school to study because I don&apos;t have
         laptop and she bought it for me.😂It was really funny how you brought
         that luck to me sweetheart. After that, we would also continue to chats.
-        I don;t really know when I fell in love with you but I know for you that
-        right now you fill my heart. We made a lot of memories such as drinking
-        beer together, do you remember that? We also went to the gym together,
-        that was fun. We ate too many thing and I ate too much that&apos;s why I
-        am this fat🤣but it is okay, those time with you were majestic and I
-        really glad I got to spend them with you. We cannot just eat a lot
-        without a care again since I want to lose weight but we will do other
-        things together. Maybe next year or a few more years, we can go far from
-        the city and go to the sea, forest, mountain, etc. You are my
+        I don&apos;t really know when I fell in love with you but I know for you
+        that right now you fill my heart. We made a lot of memories such as
+        drinking beer together, do you remember that? We also went to the gym
+        together, that was fun. We ate too many thing and I ate too much
+        that&apos;s why I am this fat🤣but it is okay, those time with you were
+        majestic and I really glad I got to spend them with you. We cannot just
+        eat a lot without a care again since I want to lose weight but we will
+        do other things together. Maybe next year or a few more years, we can go
+        far from the city and go to the sea, forest, mountain, etc. You are my
         heart&apos;s desire😘 so we will spend those time together too and I
         know it will be really fun. After I confessed to you, I really want to
         be always with you and it felt like an eternity when I am not with you.
         I still feel the same now but we are adult now we have responsibilities,
         we cannot act the same like we used to before. I believe that even
         though right now we do our own things, we will meet up once again and
-        have fun together. We just have to bear with it until then.
+        have fun together. We just have to bear with it until then. You are the
+        reason for muy happiness. You know I just want you to be always happy
+        since if you are happy, you make my world brighter too. I know there
+        will be many more problems in store for us in the future but I am sure
+        that we will go through them together. We will not lose to anything that
+        life throws at us because we are one.💝Something a little more serious
+        though, after all the things we went though, we have to look into the
+        future and try to earn money by ourselves. It will also be a long road
+        for us, even though we will walk on different path because we want to do
+        different things, we will meet once again if we accomplished our goals.
+        I want to try many different things, travelling abroad with you, would
+        be the perfect holiday. When money is not a problem to us anymore, then
+        it would be perfect. 
       </div>
     </div>
   );
